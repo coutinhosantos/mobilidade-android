bd7c28d5ecff37963ad3fb71005339a686a19858
Jefferson Coutinho <coutinho_santos@yahoo.com.br>
2024-05-23 11:32:36 -0300

    Commit inicial Mobilidade Android
    Commit inicial Mobilidade Android

---
+++ app/src/main/java/com/mobilidade/mobilidade/MainActivity.java
@@ -0,0 +1,49 @@
+package com.mobilidade.mobilidade;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final ProgressBar progressBar = findViewById(R.id.progress);
+        progressBar.setVisibility(View.INVISIBLE);
+
+        WebView webView = findViewById(R.id.webview);
+        webView.loadUrl("192.168.1.17:8081");
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                progressBar.setVisibility(View.VISIBLE); // mostra a progress
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                progressBar.setVisibility(View.INVISIBLE); // esconde a progress
+            }
+        });
+    }
+
+
+}
